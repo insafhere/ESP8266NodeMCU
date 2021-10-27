a1fe4e94d0ca042702c24ab0bc9947829dddc022
Muhamed Insaf Zainudin <84378807+insafhere@users.noreply.github.com>
2021-10-27 21:27:18 +0800

    Update nodemcumultiplexer.ino

--- nodemcumultiplexer.ino
+++ nodemcumultiplexer.ino
@@ -1,18 +1,24 @@
 #include <ESP8266WiFi.h> 
 
+//done by Muhamed Insaf
+//Using 220ohms as resistors
+
 int value1;
 int value2;
 int value3;
+int value4;
 
 int pin1=0; //GPIO10 = D1
 int pin2=4; //GPIO9 = D2
 int pin3=5; //GPIO0 = D3
+int pin4=2; //GPIO2 = D4
 
 void setup() {
   Serial.begin(9600);
   pinMode(pin1, OUTPUT);
   pinMode(pin2, OUTPUT);
   pinMode(pin3, OUTPUT);
+  pinMode(pin4, OUTPUT);
   pinMode(A0, INPUT);
 }
 
@@ -32,11 +38,20 @@ void loop() {
   value3 = analogRead(A0);
   digitalWrite(pin3, LOW);
 
+  digitalWrite(pin4, HIGH);
+  delay(100);
+  value4 = analogRead(A0);
+  digitalWrite(pin4, LOW);
+
   Serial.print("pin1=");
   Serial.println(value1);
   Serial.print("pin2=");
   Serial.println(value2);
   Serial.print("pin3=");
   Serial.println(value3);
+  Serial.print("pin4=");
+  Serial.println(value4);
+
+  //Using 220ohms as resistors
 
 }
